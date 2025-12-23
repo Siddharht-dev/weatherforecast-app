fb6d1b2f2837db891c8bf9a1830ab0e3dc65e54d
Siddharht-dev <siddharthwork1719@gmail.com>
2025-12-23 10:05:09 +0530

    Added JS

---
+++ script.js
@@ -0,0 +1,35 @@
+let userInput = document.querySelector("#input");
+let btn = document.querySelector("#btn");
+let weatherImg = document.querySelector("#image");
+let nameToDisplay = document.querySelector("#nameShow");
+let tempToDisplay = document.querySelector("#tempShow");
+let time = document.querySelector("#timeShow");
+let cloud = document.querySelector("#cloudShow");
+let speed = document.querySelector("#windShow");
+
+btn.addEventListener("click", async () => {
+    let key  = "6d4520aa91ed0dbc88a36be9ff1aae2b";
+    let URL = "https://api.openweathermap.org/data/2.5/weather?q="
+    + userInput.value + "&appid=" + key + "&units=metric"
+
+    async function getData(){
+        let res = await fetch(URL);
+        let data = await res.json();
+        return data;
+    }
+
+    let result = await getData();
+
+    let localTime = new Date((result.dt)*1000);
+
+    nameToDisplay.innerHTML = userInput.value;
+    tempToDisplay.innerHTML = result.main.temp;
+    time.innerHTML = localTime.toLocaleTimeString("en-US", {
+        hour: "numeric",
+        minute: "2-digit",
+        hour12: true
+    });
+    cloud.innerHTML = result.clouds.all + "%";
+    speed.innerHTML = result.wind.speed + "m/s";
+
+});
